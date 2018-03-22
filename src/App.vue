481f07655c498726c320ddc298f276d0016a0e81
bing8181413 <52445295@qq.com>
2018-03-23 03:12:31 +0800

    add component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <router-link to="/ha">Go to Ha</router-link>
+    <router-link to="/">Go index</router-link>
   </div>
 </template>
 
@@ -12,6 +14,12 @@ export default {
 </script>
 
 <style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
